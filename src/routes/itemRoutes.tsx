1a90c12c9c5637bc26344a89285780063aa4b847
Radek Cvejn <106878089+Tadnac@users.noreply.github.com>
2026-03-07 19:52:35 +0100

    Create itemRoutes.tsx

---
+++ src/routes/itemRoutes.tsx
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import { PrismaClient } from '@prisma/client';
+import { sendError } from '../utils/responseHelper';
+
+const router = Router();
+const prisma = new PrismaClient();
+
+router.get('/', async(req,res) => {
+
+  const {roomId,inventoryNumber} = req.query;
+  try {
+        const item = await prisma.item.findUnique({
+            where: { id: Number(id) },
+            include: { 
+                type: true,
+                room: {
+                    include: { floor: true } // U detailu chceme vědět i v jakém patře ta místnost je
+                },
+                photos: true 
+            }
+        });
+    if (!item){
+      return 
+    }
+  
+  }
+  });
