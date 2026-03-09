0d0a0fa81d488d0b11c10d165a9f85bda32d223f
Radek Cvejn <106878089+Tadnac@users.noreply.github.com>
2026-03-09 07:23:48 +0100

    add floor resolvers and floor typeDef into index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,12 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 import { buildingTypeDefs } from './graphql/typeDefs/buildingTypeDefs';
 import { buildingResolvers } from './graphql/resolvers/buildingResolver';
 
+import { floorTypeDefs } from './graphql/typeDefs/floorTypeDefs';
+import { floorResolvers } from './graphql/resolvers/floorResolver';
 
 const server = new ApolloServer({
-  typeDefs: [buildingTypeDefs], 
-  resolvers: [buildingResolvers],
+  typeDefs: [buildingTypeDefs,floorTypeDefs], 
+  resolvers: [buildingResolvers,floorResolvers],
 });
 
 const startServer = async () => {
