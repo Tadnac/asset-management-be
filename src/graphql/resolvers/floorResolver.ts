dec5a262a60e1d73ce48de357433f73d3452f8c7
Radek Cvejn <106878089+Tadnac@users.noreply.github.com>
2026-03-09 07:44:23 +0100

    Update floorResolver.ts - add resolvers for query and mutation

--- src/graphql/resolvers/floorResolver.ts
+++ src/graphql/resolvers/floorResolver.ts
@@ -1,2 +1,58 @@
 import { PrismaClient } from '@prisma/client';
 import { throwError } from '../../utils/responseHelper';
+
+const prisma = new PrismaClient();
+export const floorResolvers = {
+  
+      Query: {
+        floors: async () => {
+          try {
+            return await prisma.floor.findMany({
+              include: {building: true}
+            });
+          } catch(error) {
+            throwError('Loading floors failed');
+          }
+        },
+        floor: async (_parent: unknown, args: {id: string}) => {
+           try {
+            return await prisma.floor.findUnique({
+              where: {id: Number(args.id)},
+              include: {building: true}
+            });
+             if(!floor){
+               throwError('Floor not found', 'NOT_FOUND');
+             }
+             return floor;
+          } catch(error) {
+            throwError('Loading floors failed');
+          }
+        } 
+      },
+      Mutation: {
+         createFloor: async (_parent: unknown, args: {levelNumber: string, name?: string, buildingId: number}) => {
+           try {
+            const newFloor = await prisma.floor.create({
+              data: {
+                levelNumber: args.levelNumber,
+                name: args.name,
+                building: args.buildingId
+              }
+            });
+             return newFloor;
+          } catch(error) {
+            throwError('Creating floor failed');
+          }
+        },
+         deleteFloor: async (_parent: unknown, args: {id: string}) => {
+           try {
+            const deletedFloor = await prisma.floor.delete({
+              where: {id: Number(args.id)}
+            });
+             return deletedFloor;
+          } catch(error) {
+            throwError('Deleting floor failed');
+          }
+        }
+    }
+};
