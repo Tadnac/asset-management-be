71c50b0e1d7731ea46db183bf4a5234956e53272
Radek Cvejn <106878089+Tadnac@users.noreply.github.com>
2026-07-16 07:23:15 +0200

    Refactor PrismaClient import in floorResolver

--- src/graphql/resolvers/floorResolver.ts
+++ src/graphql/resolvers/floorResolver.ts
@@ -1,7 +1,9 @@
-import { PrismaClient } from '@prisma/client';
+import { prisma } from '../../lib/prisma';
 import { throwError } from '../../utils/responseHelper';
+import { requireRole, Context } from '../../utils/authHelper';
+import { parseId } from '../../utils/validation';
 
-const prisma = new PrismaClient();
+
 export const floorResolvers = {
   
       Query: {
